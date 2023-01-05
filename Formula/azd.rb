bbff928b36258920d9dd84e29cee0bf078661bc9
Daniel Jurek <djurek@microsoft.com>
2023-01-05 12:27:38 -0800

    version

--- Formula/azd.rb
+++ Formula/azd.rb
@@ -2,6 +2,7 @@ class Azd < Formula
   desc "Azure Developer CLI"
   homepage "https://github.com/azure/azure-dev"
   url "https://github.com/Azure/azure-dev/releases/download/azure-dev-cli_0.4.0-beta.1/azd-darwin-amd64.zip"
+  version "0.4.0-beta.1"
   sha256 "141657238c75f314ab049904ef3ce031ff3def69834bde57066f22e15d8698b6"
   license "MIT"
 
